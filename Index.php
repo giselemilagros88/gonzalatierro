fbc0dfba24998ec2b4e38c2cbc32096dfa0f89f0
giselemilagros88 <92891602+giselemilagros88@users.noreply.github.com>
2022-03-10 14:34:53 -0300

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Amba Beer</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Festive&family=Nunito:wght@200&family=Open+Sans:ital,wght@1,300&family=Roboto+Condensed:wght@300&family=Rowdies:wght@300&display=swap"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+
+</head>
+
+<body class="container_1">
+    <header>
+        <div class="div_logo">
+            <a href="Index.html">
+                AMBA BEER
+            </a>
+        </div>
+        <div class="div_enlaces">
+            <a href="Index.html">Inicio</a>
+            <a href="Nosotros.html">Nosotros</a>
+            <a href="Cursos.html">Cursos</a>
+            <a href="Productos.html">Productos</a>
+            <a href="Contacto.html">Contacto</a>
+        </div>
+    </header>
+
+    <main>
+        <div class="container-fluid"></div>
+        <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="imagenes/4-Estilos-Cerveceros-Cultura.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="imagenes/dia-del-trabajador-cervecero_5.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="imagenes/e23d6c_6d696c56bed54fb3bccde9628d0778ef_mv2233.jpg" class="d-block w-100"
+                        alt="...">
+                </div>
+            </div>
+        </div>
+        </div>
+        <div class="color_titulo">
+            <div>
+                <br>
+                <br>
+                <br>
+                <h1>AMBA BEER</h1>
+            </div>
+            <div>
+                <h2>CERVEZA ARTESANAL</h2>
+                <br>
+                <br>
+                <br>
+            </div>
+        </div>
+        <section class="section_main_index">
+            <div class="div_individual_index">
+                <li><img src="./imagenes/Nosotros.png" width="380" height="330"></li>
+                <br>
+                <h3>Sobre Nosotros</h3>
+                <br>
+                <p>Somos una Pyme familiar comprometidos con nuestro trabajo, nuestra mision es poder lograr la
+                    mejor
+                    cerveza artesanal de argentina. Conoce mas de nuestra historia</p>
+            </div>
+            <div class="div_individual_index">
+                <li><img src="https://static.vecteezy.com/system/resources/previews/001/894/710/non_2x/bottle-and-mug-glass-of-beer-on-white-background-free-vector.jpg"
+                        width="400" height="330"></li>
+                <br>
+                <h3>Nuestras Cervezas </h3>
+                <br>
+                <p>Proba nuestras cervezas artesanales elaboradas con la mejores materias primas del mercado. Somos
+                    Productores de cervezas rubias - Rojas - Negras</p>
+            </div>
+            <div class="div_individual_index">
+                <li><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ7vUXkwTYcWWUsaDmPPZiDyTx94YOs2D3sCCCY844lRTi9qBni9HiEHkY9UtBNDwC6K4Q&usqp=CAU"
+                        width="330" height="330"></li>
+                <br>
+                <h3>Elabora tu propia Cerveza</h3>
+                <br>
+                <p>Veni con nosotros y aprende a realizar tu propia cerveza, te explicamos el paso a paso de la
+                    elaboracion de la cerveza artesanal para que la puedas realizar en el patio de tu casa y
+                    disfrutarla
+                    con familiares y amigos.</p>
+            </div>
+        </section>
+    </main>
+    <footer>
+        <div class="div_enlaces">
+            <a href="Index.html">Inicio</a>
+            <a href="Nosotros.html">Nosotros</a>
+            <a href="Cursos.html">Cursos</a>
+            <a href="Productos.html">Productos</a>
+            <a href="Contacto.html">Contacto</a>
+        </div>
+        <br>
+        <div>
+            <ul>
+                <a href="https://www.linkedin.com/in/gonzalo-latierro-14470729/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BqFKubIFHRXaZG9b%2FJb%2Bgrw%3D%3D"
+                    target="_blank">
+                    <img src="https://kimglobal.com/wp-content/uploads/2016/10/LinkedIn-simbolo.png" width="40"
+                        height="40" alt="Linkedin gonza">
+                </a>
+                <a href="https://www.linkedin.com/in/gonzalo-latierro-14470729/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BqFKubIFHRXaZG9b%2FJb%2Bgrw%3D%3D"
+                    target="_blank">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1409/1409946.png" width="40" height="40"
+                        alt="Linkedin gonza">
+                </a>
+                <a href="https://www.linkedin.com/in/gonzalo-latierro-14470729/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BqFKubIFHRXaZG9b%2FJb%2Bgrw%3D%3D"
+                    target="_blank">
+                    <img src="https://st2.depositphotos.com/1116329/7330/v/600/depositphotos_73305645-stock-illustration-vector-modern-phone-icon-in.jpg"
+                        width="40" height="40" alt="Linkedin gonza">
+                </a>
+            </ul>
+        </div>
+        <br>
+        <div>
+            <p>@Gonzalo Latierro todos los derechos reservados</p>
+        </div>
+    </footer>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ Nosotros.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sobre Nosotros</title>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Festive&family=Open+Sans:ital,wght@1,300&family=Roboto+Condensed:wght@300&family=Rowdies:wght@300&display=swap"
+        rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="shortcut icon" href="./imagenes/favicom.png">
+</head>
+
+<body class="container_1">
+    <header>
+        <div class="div_logo">
+            <a href="Index.html">
+                AMBA BEER
+            </a>
+        </div>
+        <div class="div_enlaces">
+            <a href="Index.html">Inicio</a>
+            <a href="Nosotros.html">Nosotros</a>
+            <a href="Cursos.html">Cursos</a>
+            <a href="Productos.html">Productos</a>
+            <a href="Contacto.html">Contacto</a>
+        </div>
+    </header>
+
+    <main class="main_nosotros">
+        <div>
+            <h1>
+                <br>
+                Sobre Nosotros
+                <br>
+            </h1>
+        </div>
+        <div>
+            <br>
+            <br>
+            <p>Somos una Pyme familiar que empezo en 2010 comprometidos con nuestro trabajo, nuestra mision es poder
+                lograr la mejor
+                cerveza artesanal de argentina.</p>
+            <p>Nuestro Metodo 100% artesanal nos permite entregar a nuestros clientes un producto de gran calidad
+                utilizando las mejores materias primas del
+                Mercado.
+            </p>
+            <p>
+                Nos dedicamos a la produccion y comercializacion de las siguientes cervezas: Rubias - Rojas -
+                Negras.
+                <br>
+                <br>
+            </p>
+        </div>
+        <div>
+            <li class="fotos">
+                <img src="./imagenes/Header-1200x800.png" width="500" height="330">
+                <img src="./imagenes/SMM6018-1-1394x800.jpg" width="500" height="330">
+                <img src="./imagenes/30057063_ml.jpg" width="500" height="330">
+            </li>
+            <li class="fotos">
+                <img src="./imagenes/MALTA-1000x565.jpeg" width="500" height="330">
+                <img src="./imagenes/dc38996d-da62-4fab-8316-7fb2a1c14e78-600-1.jpg" width="500" height="330">
+                <img src="./imagenes/cerveza-artesanal-mercado-con-crecimiento-en-mexico.jpg" width="500" height="330">
+            </li>
+
+        </div>
+    </main>
+    <footer>
+        <div class="div_enlaces">
+            <a href="Index.html">Inicio</a>
+            <a href="Nosotros.html">Nosotros</a>
+            <a href="Cursos.html">Cursos</a>
+            <a href="Productos.html">Productos</a>
+            <a href="Contacto.html">Contacto</a>
+        </div>
+        <br>
+        <div>
+            <ul>
+                <a href="https://www.linkedin.com/in/gonzalo-latierro-14470729/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BqFKubIFHRXaZG9b%2FJb%2Bgrw%3D%3D"
+                    target="_blank">
+                    <img src="https://kimglobal.com/wp-content/uploads/2016/10/LinkedIn-simbolo.png" width="40"
+                        height="40" alt="Linkedin gonza">
+                </a>
+                <a href="https://www.linkedin.com/in/gonzalo-latierro-14470729/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BqFKubIFHRXaZG9b%2FJb%2Bgrw%3D%3D"
+                    target="_blank">
+                    <img src="https://cdn-icons-png.flaticon.com/512/1409/1409946.png" width="40" height="40"
+                        alt="Linkedin gonza">
+                </a>
+                <a href="https://www.linkedin.com/in/gonzalo-latierro-14470729/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BqFKubIFHRXaZG9b%2FJb%2Bgrw%3D%3D"
+                    target="_blank">
+                    <img src="https://st2.depositphotos.com/1116329/7330/v/600/depositphotos_73305645-stock-illustration-vector-modern-phone-icon-in.jpg"
+                        width="40" height="40" alt="Linkedin gonza">
+                </a>
+            </ul>
+        </div>
+        <br>
+        <div>
+            <p>@Gonzalo Latierro todos los derechos reservados...</p>
+        </div>
+    </footer>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
